524e5af9b48dbb618f13bf96422a72b185e12b36
trqlong26 <longqtr2608@gmail.com>
2025-06-18 04:32:03 +0700

    feat: add workflow

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -8,8 +8,12 @@ const config = {
     },
     databaseUrl: process.env.DATABASE_URL!,
     upstash: {
-      redisUrl: process.env.UPSTASH_REDIS_REST_URL,
-      redisToken: process.env.UPSTASH_REDIS_REST_TOKEN,
+      redisUrl: process.env.UPSTASH_REDIS_REST_URL!,
+      redisToken: process.env.UPSTASH_REDIS_REST_TOKEN!,
+      qstashUrl: process.env.QSTASH_URL!,
+      qstashToken: process.env.QSTASH_TOKEN!,
+      qstashCurrentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY!,
+      qstashNextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY!,
     },
   },
 };
